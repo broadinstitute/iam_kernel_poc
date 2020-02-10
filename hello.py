f5191712efd2e4a3b32db5041ab93f040154a3b7
yonghao <yyhao1@gmail.com>
2020-02-10 13:04:06 -0500

    add two API calls for kernel and core layers.

--- hello.py
+++ hello.py
@@ -3,6 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/core", method=['GET'])
+# core layer api to get all workspaces
+def workspace():
+    return "workspace found in core layer!"
+
+
+@app.route("/kernel", method=['GET'])
+# kernel layer api to get all workspaces
+def workspace():
+    return "I am kernel"
