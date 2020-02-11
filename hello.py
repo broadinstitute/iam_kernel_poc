8ccb25b0b99e9821fb7066d2fa247fa318697c33
yonghao <yyhao1@gmail.com>
2020-02-11 11:17:11 -0500

    fix few exceptions in current app

--- hello.py
+++ hello.py
@@ -3,13 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/core", method=['GET'])
+@app.route("/")
 # core layer api to get all workspaces
-def workspace():
-    return "workspace found in core layer!"
+def root():
+    return "Hello World!"
+
+@app.route("/core")
+# core layer api to get all workspaces
+def core():
+    return "I am core!"
 
 
-@app.route("/kernel", method=['GET'])
+@app.route("/kernel")
 # kernel layer api to get all workspaces
-def workspace():
-    return "I am kernel"
+def kernel():
+    return "I am kernel!"
