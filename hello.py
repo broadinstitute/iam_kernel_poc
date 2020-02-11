ed46a222498052c74c16d40a0efd3c13cedcb24d
yonghao <yyhao1@gmail.com>
2020-02-11 14:19:04 -0500

    add chained endpoints

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+_KERNEL1_URL = 'localhost:5000'
+
+_KERNEL2_URL = 'localhost:5000'
+
+KERNEL_PATH = "/kernel"
 
 @app.route("/")
 # core layer api to get all workspaces
 def root():
     return "Hello World!"
+
 
 @app.route("/core")
 # core layer api to get all workspaces
@@ -18,3 +25,17 @@ def core():
 # kernel layer api to get all workspaces
 def kernel():
     return "I am kernel!"
+
+
+@app.route("/coreToKernel1")
+# kernel layer api to get all workspaces
+def coreToKernel1():
+    kernelResponse = requests.get(_KERNEL1_URL + KERNEL_PATH)
+    return "Core to kernel1 " + kernelResponse;
+
+
+@app.route("/coreToKernel2")
+# kernel layer api to get all workspaces
+def coreToKernel2():
+    kernelResponse = requests.get(_KERNEL2_URL + KERNEL_PATH)
+    return "Core to kernel2 " +  kernelResponse
